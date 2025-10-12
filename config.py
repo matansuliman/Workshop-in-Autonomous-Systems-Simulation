e33fb0f8338878ee0df44894c08622ed3613a668
matansuliman <matan.2001@gmail.com>
2025-10-12 14:36:04 +0300

    remove empty lines from config profile

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 import yaml
 
 class ConfigProfile:
-
     def __init__(self, filename= 'config.yaml'):
         with open(filename, "r") as f:
             self._profile = yaml.safe_load(f)
 
     def get_profile(self):
         return self._profile
-
 
 
 CONFIG = ConfigProfile().get_profile()
