97835eddd515eeadaa73f574c781d6336ef52867
matansuliman <matan.2001@gmail.com>
2025-10-16 11:44:29 +0300

    fix path comment

--- core/LowLevel/SimulationDecoupling/backend.py
+++ core/LowLevel/SimulationDecoupling/backend.py
@@ -1,4 +1,4 @@
-# core/backends/base_backend.py
+# core/LowLevel/SimulationDecoupling/backend.py
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Iterable, Optional, Dict
