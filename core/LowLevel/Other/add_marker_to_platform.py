95c13f2a4c026495d914bd3b18bfbd94d80f1614
matansuliman <matan.2001@gmail.com>
2025-10-16 10:36:52 +0300

    update new name of files

--- core/LowLevel/Other/add_marker_to_platform.py
+++ core/LowLevel/Other/add_marker_to_platform.py
@@ -3,9 +3,9 @@ from PIL import Image
 N = 200
 M = N * 2
 
-base = Image.open("skydio_x2/assets/donut_texture.png")
-marker = Image.open("skydio_x2/assets/4x4_1000-0.png").resize((M, M))
+base = Image.open("Assets/skydio_x2/assets/donut_texture.png")
+marker = Image.open("Assets/skydio_x2/assets/ArUco_marker_4x4_1000-0.png").resize((M, M))
 
 base.paste(marker, (462 - N, 462 - N))
 
-base.save("donut_with_markers.png")
+base.save("donut_with_marker.png")
