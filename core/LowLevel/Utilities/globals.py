6e2d02fd0dc63d80da3378c7de4a394e0d3ff6ae
matansuliman <matan.2001@gmail.com>
2025-10-20 02:51:11 +0300

    add global var timestamp of current run

--- core/LowLevel/Utilities/globals.py
+++ core/LowLevel/Utilities/globals.py
@@ -1,4 +1,6 @@
 # core/LowLevel/Utilities/globals.py
+import datetime
+
 from ...LowLevel.SimulationDecoupling.environment import ENV
 from ...LowLevel.RuntimeOutputs.logger import setup_logger
 from ...LowLevel.Utilities.config import CONFIG
@@ -11,3 +13,5 @@ LOGGER = setup_logger(
     datefmt=CONFIG["logger"]["datefmt"]
 )
 ENVIRONMENT = ENV(CONFIG["path_to_xml"])
+
+RUN_TIMESTAMP = datetime.datetime.now().strftime("%H_%M_%S__%Y_%m_%d")
